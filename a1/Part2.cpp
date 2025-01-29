1d497b884760ff9fec353c99c7eb495191d63d80
ChiefsBestPal <67126866+ChiefsBestPal@users.noreply.github.com>
2025-01-29 01:58:59 -0500

    Refactored class+func definitions to header files prototypes

--- a1/Part2.cpp
+++ a1/Part2.cpp
@@ -2,199 +2,187 @@
 
 using namespace std;
 
-// Point class
-class Point {
-private:
-    int x, y, z;
+// Point class implementation
 
-public:
+// Constructor
+Point::Point(int x, int y, int z) : x(x), y(y), z(z) {}
 
-    // Constructor
-    Point(int x = 0, int y = 0, int z = 0) : x(x), y(y), z(z) {}
+// Destructor (default, as no dynamic memory needs to be freed from heap)
+Point::~Point() {}
 
-    // Destructor (default, as no dynamic memory needs to be freed from heap)
-    ~Point() {}
+// Getters
+int Point::getX() const { return x; }
+int Point::getY() const { return y; }
+int Point::getZ() const { return z; }
 
-    // Getters
-    int getX() const { return x; }
-    int getY() const { return y; }
-    int getZ() const { return z; }
-
-    // Translate function to move point along specified axis
-    int translate(int d, char axis) {
-        switch(axis) {
-            case 'x': x += d; break;
-            case 'y': y += d; break;
-            case 'z': z += d; break;
-            default: return -1;  // Invalid axis value
-        }
-        return 0;  // Successful translation
+// Translate function to move point along specified axis
+int Point::translate(int d, char axis) {
+    switch(axis) {
+        case 'x': x += d; break;
+        case 'y': y += d; break;
+        case 'z': z += d; break;
+        default: return -1;  // Invalid axis value
     }
+    return 0;  // Successful translation
+}
 
-    // Display function to show point coordinates
-    string toString() const {
-        stringstream ss;
-        ss << "Point(" << x << "," << y << "," << z << ")";
-        return ss.str();
+// Display function to show point coordinates
+string Point::toString() const {
+    stringstream ss;
+    ss << "Point(" << x << "," << y << "," << z << ")";
+    return ss.str();
+}
+
+// Distance between this point and another point
+inline double Point::distanceTo(const Point& other) const {
+    return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2) + pow(z - other.z, 2));
+}
+
+// Operator overload for vector operations
+Point Point::operator+(const Point& other) const {return {x + other.x, y + other.y, z + other.z};}
+Point Point::operator-(const Point& other) const {return {x - other.x, y - other.y, z - other.z};}
+Point Point::operator*(int scalar) const {return {x * scalar, y * scalar, z * scalar};}
+Point Point::operator/(int scalar) const { return {x / scalar, y / scalar, z / scalar};}
+// Operator overload for boolean comparison operations
+bool Point::operator==(const Point& other) const { return x == other.x && y == other.y && z == other.z; }
+bool Point::operator!=(const Point& other) const { return !(*this == other); }
+
+// Tuple conversion for easy unpacking of coordinates
+inline tuple<int, int, int> Point::as_tuple() const {return make_tuple(x, y, z);}
+
+// Non-member friend function ostream overload for easy printing of Point objects
+ostream& operator<<(ostream& os, const Point& point) { return os << point.toString(); }
+
+
+
+// Triangle class implementation
+
+// Explicit Constructor with three Point objects
+Triangle::Triangle(const Point& v1, const Point& v2, const Point& v3) {
+    if (!Triangle::isValidTrianglePoints(v1,v2,v3)) {
+        throw logic_error("The points do not form a valid triangle.");
     }
+    // Dynamically allocate a copy of the Point object referenced and assign new pointer to it
+    vertex_1 = new Point(v1);
+    vertex_2 = new Point(v2);
+    vertex_3 = new Point(v3);
+}
 
-    // Distance between this point and another point
-    inline double distanceTo(const Point& other) const {
-        return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2) + pow(z - other.z, 2));
-    }
+// Default constructor, setting pointers to null
+Triangle::Triangle() : vertex_1(nullptr), vertex_2(nullptr), vertex_3(nullptr) {}
 
-    // Operator overload for vector operations
-    Point operator+(const Point& other) const {return {x + other.x, y + other.y, z + other.z};}
-    Point operator-(const Point& other) const {return {x - other.x, y - other.y, z - other.z};}
-    Point operator*(int scalar) const {return {x * scalar, y * scalar, z * scalar};}
-    Point operator/(int scalar) const { return {x / scalar, y / scalar, z / scalar};}
-    // Operator overload for boolean comparison operations
-    bool operator==(const Point& other) const { return x == other.x && y == other.y && z == other.z; }
-    bool operator!=(const Point& other) const { return !(*this == other); }
-
-    // Tuple conversion for easy unpacking of coordinates
-    tuple<int, int, int> as_tuple() const {return make_tuple(x, y, z);}
-
-    // Non-member function ostream overload for easy printing of Point objects
-    friend ostream& operator<<(ostream& os, const Point& point) { return os << point.toString(); }
-
-
-};
-
-// Triangle class
-class Triangle {
-private:
-    Point* vertex_1;
-    Point* vertex_2;
-    Point* vertex_3;
-public:
-    // Constructor with three Point objects
-    explicit Triangle(const Point& v1, const Point& v2, const Point& v3) {
-        if (!Triangle::isValidTrianglePoints(v1,v2,v3)) {
-            throw logic_error("The points do not form a valid triangle.");
-        }
-        // Dynamically allocate a copy of the Point object referenced and assign new pointer to it
-        vertex_1 = new Point(v1);
-        vertex_2 = new Point(v2);
-        vertex_3 = new Point(v3);
-    }
-
-    // Default constructor, setting pointers to null
-    Triangle() : vertex_1(nullptr), vertex_2(nullptr), vertex_3(nullptr) {}
-
-    // Move constructor
+// Move constructor
 //    Triangle(Triangle&& other) noexcept
 //            : vertex_1(other.vertex_1), vertex_2(other.vertex_2), vertex_3(other.vertex_3) {
 //        other.vertex_1 = other.vertex_2 = other.vertex_3 = nullptr;
 //    }
 
-    // Clone function to create a deep copy of the triangle
-    Triangle* clone() const {
-        return new Triangle(*vertex_1, *vertex_2, *vertex_3);
+// Clone function to create a deep copy of the triangle
+Triangle* Triangle::clone() const {
+    return new Triangle(*vertex_1, *vertex_2, *vertex_3);
+}
+
+// Prevent Copy constructor
+// Each Triangle is a uniquely labeled mesh in 3D space
+// (In favor of move semantics or deep copy clone)
+// Triangle(const Triangle&) = delete;
+// Move ptr assignment constructor if need remove: Triangle& operator=(const Triangle&) = delete;
+
+// Destructor (freeing dynamically allocated memory i.e. the 3 raw vertices pointers)
+Triangle::~Triangle() {
+    delete vertex_1;
+    delete vertex_2;
+    delete vertex_3;
+}
+
+// Getter (Use point index for less verbosity)
+Point* Triangle::getVertex(int index) const {
+    switch (index) {
+        case 1: return vertex_1;
+        case 2: return vertex_2;
+        case 3: return vertex_3;
+        default: return nullptr;
+    }
+}
+// Setters (Use point index for less verbosity)
+void Triangle::setVertex(int index, const Point& v) {
+    Point* vertices[] = {vertex_1, vertex_2, vertex_3};
+    int v1 = (index + 1) % 3, v2 = (index + 2) % 3;
+    if (!Triangle::isValidTrianglePoints(v, *vertices[v1], *vertices[v2])) {
+        throw invalid_argument("The new point does not form a valid triangle.");
+    }
+    *vertices[index] = v;
+}
+// Translate function for the entire triangle
+void Triangle::translate(int d, char axis) {
+    if (vertex_1) vertex_1->translate(d, axis);
+    if (vertex_2) vertex_2->translate(d, axis);
+    if (vertex_3) vertex_3->translate(d, axis);
+
+    if (!Triangle::isValidTrianglePoints(*vertex_1,*vertex_2,*vertex_3)) {
+        throw logic_error("The points after transform do not form a valid triangle.");
+    }
+}
+
+/*
+ * Returns the area of the triangle using the cross product magnitude method in 3D Euclidean space
+ *
+ * Area of a triangle with vertices A(x1, y1, z1), B(x2, y2, z2), C(x3, y3, z3) is given by:
+ * 0.5 * |AB x AC|
+ */
+double Triangle::calcArea() const {
+    if (!vertex_1 || !vertex_2 || !vertex_3) return 0.0;
+
+    // Using the cross product method to calculate area of triangle in R3 space
+    auto [x1, y1, z1] = vertex_1->as_tuple();
+    auto [x2, y2, z2] = vertex_2->as_tuple();
+    auto [x3, y3, z3] = vertex_3->as_tuple();
+
+    // Vectors AB and AC
+    int ABx = x2 - x1, ABy = y2 - y1, ABz = z2 - z1;
+    int ACx = x3 - x1, ACy = y3 - y1, ACz = z3 - z1;
+
+    // Cross product of AB and AC by 3x3 matrix determinant
+    long long cross_x = static_cast<long long>(ABy) * ACz - static_cast<long long>(ABz) * ACy;
+    long long cross_y = static_cast<long long>(ABz) * ACx - static_cast<long long>(ABx) * ACz;
+    long long cross_z = static_cast<long long>(ABx) * ACy - static_cast<long long>(ABy) * ACx;
+
+    // Area of triangle is half the magnitude of the cross product AB x AC
+    // (Since that cross product is a normal vector to the triangle plane and that vector's magnitude is the area of the parallelogram formed by the sides vectors)
+    long long cross_product_magnitude_squared = cross_x * cross_x + cross_y * cross_y + cross_z * cross_z;
+    if (cross_product_magnitude_squared < 0) {
+        throw overflow_error("Overflow occurred during area calculation.");
     }
 
-    // Prevent Copy constructor
-    // Each Triangle is a uniquely labeled mesh in 3D space
-    // (In favor of move semantics or deep copy clone)
-    Triangle(const Triangle&) = delete;
-    // Move ptr assignment constructor if need remove: Triangle& operator=(const Triangle&) = delete;
-
-    // Destructor (freeing dynamically allocated memory i.e. the 3 raw vertices pointers)
-    ~Triangle() {
-        delete vertex_1;
-        delete vertex_2;
-        delete vertex_3;
+    double area = 0.5 * sqrt(static_cast<double>(cross_product_magnitude_squared));
+    if (isinf(area) || isnan(area)) {
+        throw runtime_error("Invalid numerical area calculation.");
     }
 
-    // Getter (Use point index for less verbosity)
-    Point* getVertex(int index) const {
-        switch (index) {
-            case 1: return vertex_1;
-            case 2: return vertex_2;
-            case 3: return vertex_3;
-            default: return nullptr;
-        }
-    }
-    // Setters (Use point index for less verbosity)
-    void setVertex(int index, const Point& v) {
-        Point* vertices[] = {vertex_1, vertex_2, vertex_3};
-        int v1 = (index + 1) % 3, v2 = (index + 2) % 3;
-        if (!Triangle::isValidTrianglePoints(v, *vertices[v1], *vertices[v2])) {
-            throw invalid_argument("The new point does not form a valid triangle.");
-        }
-        *vertices[index] = v;
-    }
-    // Translate function for the entire triangle
-    void translate(int d, char axis) {
-        if (vertex_1) vertex_1->translate(d, axis);
-        if (vertex_2) vertex_2->translate(d, axis);
-        if (vertex_3) vertex_3->translate(d, axis);
+    return area;
+}
 
-        if (!isValidTrianglePoints(*vertex_1,*vertex_2,*vertex_3)) {
-            throw logic_error("The points after transform do not form a valid triangle.");
-        }
-    }
+// Display function to show triangle coordinates
+string Triangle::toString() const {
+    stringstream ss;
+    ss << "Triangle: {\n  Vertex1: (" << vertex_1->getX() << "," << vertex_1->getY() << "," << vertex_1->getZ() << ")\n"
+       << "  Vertex2: (" << vertex_2->getX() << "," << vertex_2->getY() << "," << vertex_2->getZ() << ")\n"
+       << "  Vertex3: (" << vertex_3->getX() << "," << vertex_3->getY() << "," << vertex_3->getZ() << ")\n}";
+    return ss.str();
+}
+// Non-member friend function ostream overload for easy printing of Triangle objects
+ostream& operator<<(ostream& os, const Triangle& tri) { return os << tri.toString();}
 
-    /*
-     * Returns the area of the triangle using the cross product magnitude method in 3D Euclidean space
-     *
-     * Area of a triangle with vertices A(x1, y1, z1), B(x2, y2, z2), C(x3, y3, z3) is given by:
-     * 0.5 * |AB x AC|
-     */
-    double calcArea() const {
-        if (!vertex_1 || !vertex_2 || !vertex_3) return 0.0;
-
-        // Using the cross product method to calculate area of triangle in R3 space
-        auto [x1, y1, z1] = vertex_1->as_tuple();
-        auto [x2, y2, z2] = vertex_2->as_tuple();
-        auto [x3, y3, z3] = vertex_3->as_tuple();
-
-        // Vectors AB and AC
-        int ABx = x2 - x1, ABy = y2 - y1, ABz = z2 - z1;
-        int ACx = x3 - x1, ACy = y3 - y1, ACz = z3 - z1;
-
-        // Cross product of AB and AC by 3x3 matrix determinant
-        long long cross_x = static_cast<long long>(ABy) * ACz - static_cast<long long>(ABz) * ACy;
-        long long cross_y = static_cast<long long>(ABz) * ACx - static_cast<long long>(ABx) * ACz;
-        long long cross_z = static_cast<long long>(ABx) * ACy - static_cast<long long>(ABy) * ACx;
-
-        // Area of triangle is half the magnitude of the cross product AB x AC
-        // (Since that cross product is a normal vector to the triangle plane and that vector's magnitude is the area of the parallelogram formed by the sides vectors)
-        long long cross_product_magnitude_squared = cross_x * cross_x + cross_y * cross_y + cross_z * cross_z;
-        if (cross_product_magnitude_squared < 0) {
-            throw overflow_error("Overflow occurred during area calculation.");
-        }
-
-        double area = 0.5 * sqrt(static_cast<double>(cross_product_magnitude_squared));
-        if (isinf(area) || isnan(area)) {
-            throw runtime_error("Invalid numerical area calculation.");
-        }
-
-        return area;
-    }
-
-    // Display function to show triangle coordinates
-    string toString() const {
-        stringstream ss;
-        ss << "Triangle: {\n  Vertex1: (" << vertex_1->getX() << "," << vertex_1->getY() << "," << vertex_1->getZ() << ")\n"
-           << "  Vertex2: (" << vertex_2->getX() << "," << vertex_2->getY() << "," << vertex_2->getZ() << ")\n"
-           << "  Vertex3: (" << vertex_3->getX() << "," << vertex_3->getY() << "," << vertex_3->getZ() << ")\n}";
-        return ss.str();
-    }
-    // Non-member function ostream overload for easy printing of Triangle objects
-    friend ostream& operator<<(ostream& os, const Triangle& tri) { return os << tri.toString();}
-
-    // Utility Function to check if the points form a valid triangle using inequality theorem
-    static bool isValidTrianglePoints(const Point& v1, const Point& v2, const Point& v3) {
-        constexpr double EPSILON = 1e-9;
-        double a = v1.distanceTo(v2);
-        double b = v2.distanceTo(v3);
-        double c = v3.distanceTo(v1);
-        return a + b > c + EPSILON && b + c > a + EPSILON && c + a > b + EPSILON;
-    }
+// Utility static Function to check if the points form a valid triangle using inequality theorem
+bool Triangle::isValidTrianglePoints(const Point& v1, const Point& v2, const Point& v3) {
+    constexpr double EPSILON = 1e-9;
+    double a = v1.distanceTo(v2);
+    double b = v2.distanceTo(v3);
+    double c = v3.distanceTo(v1);
+    return a + b > c + EPSILON && b + c > a + EPSILON && c + a > b + EPSILON;
+}
 
 
-};
 
 // Driver code
 // TODO Make better menu system with options to retry
--- a1/Part2.h
+++ a1/Part2.h
@@ -3,10 +3,95 @@
 
 #include <iostream>
 #include <sstream>
-#include <cmath>
+#include <stdexcept>
 #include <tuple>
-#include <memory>
+#include <cmath>
+#include <string>
 
-int part2_main();
+class Point {
+private:
+    int x, y, z;
 
-#endif //PART2_H
+public:
+    // Constructor
+    Point(int x = 0, int y = 0, int z = 0);
+
+    // Destructor
+    ~Point();
+
+    // Getters
+    int getX() const;
+    int getY() const;
+    int getZ() const;
+
+    // Translate function
+    int translate(int d, char axis);
+
+    // Display function
+    std::string toString() const;
+
+    // Distance between this point and another point
+    inline double distanceTo(const Point& other) const;
+
+    // Operator overloads for vector operations
+    Point operator+(const Point& other) const;
+    Point operator-(const Point& other) const;
+    Point operator*(int scalar) const;
+    Point operator/(int scalar) const;
+
+    // Comparison operators
+    bool operator==(const Point& other) const;
+    bool operator!=(const Point& other) const;
+
+    // Convert to tuple
+    inline std::tuple<int, int, int> as_tuple() const;
+
+    // Stream output operator
+    friend std::ostream& operator<<(std::ostream& os, const Point& point);
+};
+
+class Triangle {
+private:
+    Point* vertex_1;
+    Point* vertex_2;
+    Point* vertex_3;
+
+public:
+    // Constructors
+    explicit Triangle(const Point& v1, const Point& v2, const Point& v3);
+    Triangle();
+
+    // Prevent Copy constructor
+    // Each Triangle is a uniquely labeled mesh in 3D space
+    // (In favor of move semantics or deep copy clone)
+    Triangle(const Triangle&) = delete;
+    // Move ptr assignment constructor if need remove: Triangle& operator=(const Triangle&) = delete;
+
+
+    // Destructor
+    ~Triangle();
+
+    // Clone function
+    Triangle* clone() const;
+
+    // Getters and Setters for vertices
+    Point* getVertex(int index) const;
+    void setVertex(int index, const Point& v);
+
+    // Translate function
+    void translate(int d, char axis);
+
+    // Calculate area
+    double calcArea() const;
+
+    // Display function
+    std::string toString() const;
+
+    // Stream output operator
+    friend std::ostream& operator<<(std::ostream& os, const Triangle& tri);
+
+    // Utility function to check for a valid triangle
+    static bool isValidTrianglePoints(const Point& v1, const Point& v2, const Point& v3);
+};
+
+#endif // PART2_H
